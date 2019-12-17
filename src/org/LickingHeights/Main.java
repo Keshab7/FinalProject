387c6427edbda0107feaf688144a7da2c51b416f
Keshab <keshab.bista.2020@student.lhschools.org>
2019-12-17 10:00:01 -0500

    Done

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,8 +1,23 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       fahrenheitToCelsius();
+    }
+    public static void fahrenheitToCelsius() {
+        Scanner keyboard;
+        double Fahrenheit;
+        double Celsius;
+        keyboard = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Give me a Fahrenheit value");
+            Fahrenheit = keyboard.nextDouble();
+            Celsius = (Fahrenheit - 32) / 1.8;
+            System.out.println(Fahrenheit+" Degrees Fahrenheit is " + Celsius +  " Degrees Celsius" );
+        }
     }
 }
